ee80c5c3e6e660be809962eb76880d8d33646427
Bruce Evans <bruce.evans.dev@gmail.com>
2025-04-29 20:00:42 +0900

    Prep for Network Access Cleaning up the ContentView before we add network access.

--- GithubViewer/Views/ContentView.swift
+++ GithubViewer/Views/ContentView.swift
@@ -10,16 +10,18 @@ import SwiftUI
 struct ContentView: View {
     @State var authenticationModel = AuthenticationModel()
     
-    @State private var showAuthenticationPopup: Bool = false
-    
     var body: some View {
         switch authenticationModel {
         case .none:
             AuthenticationInputView { newModel in
                 authenticationModel = newModel
             }
-        case .some(let token):
-            Text("Logged in with \(token)")
+        case .some(let model):
+            ProgressView()
+                .foregroundStyle(.primary)
+                .padding()
+                .background(.tertiary)
+                .cornerRadius(15)
         }
     }
 }
